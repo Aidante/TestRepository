cc52a5d81d156586a0a47d1789ad446e5bbe7d90
Aidante <aidanjt@rogers.com>
2016-06-09 14:12:27 -0400

    Update Testing.c

--- Testing.c
+++ Testing.c
@@ -1,13 +1,11 @@
+//Include statements
 #include <stdio.h>
 #include <stdlib.h>
-//Include statements
 
 //Main method
 int main() {
   //Most stereotypical test
   printf("Hello World?");
-  //This is another comment
-  //This is a rather useless return statement
+  //Return no error
   return 0;
 }
-//This is the end!  :D
